198dbe604f16a89cbab618c7cc29aafd94e1b2ac
Matheus Fernandes Scola <matheus.fernan01@gmail.com>
2023-07-17 12:09:25 -0300

    Inclusão de critério para ordenação dos clientes.

--- clientes.php
+++ clientes.php
@@ -4,7 +4,7 @@
 
 include("conexao.php");
 
-$sql_code = "SELECT * FROM clientes";
+$sql_code = "SELECT * FROM clientes ORDER BY id ASC ";
 $query_clientes = $mysqli->query($sql_code) or die($mysqli->error);
 $num_clientes = $query_clientes->num_rows;
 
